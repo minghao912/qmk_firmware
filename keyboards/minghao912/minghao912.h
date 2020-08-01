1c62f7b061c569f4352cf207464c88175aae9a97
Ming <36557541+minghao912@users.noreply.github.com>
2020-07-31 18:25:13 -0700

    Formatting

--- keyboards/minghao912/minghao912.h
+++ keyboards/minghao912/minghao912.h
@@ -1,9 +1,31 @@
+/* Copyright 2020 minghao912
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #pragma once
-
 #include "quantum.h"
 
 #define _____ KC_NO
 
+/* This a shortcut to help you visually see your layout.
+ *
+ * The first section contains all of the arguments representing the physical
+ * layout of the board and position of the keys.
+ *
+ * The second converts the arguments into a two-dimensional array which
+ * represents the switch matrix.
+ */
 #define LAYOUT( \
 	K000,       K002, K003, K004, K005,       K007, K008, K009, K010, K011, K012, K013, K014, 		K015, K016, K017, \
 	                                                                                                            	  \
